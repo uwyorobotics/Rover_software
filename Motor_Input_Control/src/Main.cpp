8b1d91105ce220158cfd7985c85e84e402af21ff
Zach Nelson <zacharynelsonwyo@gmail.com>
2026-03-03 19:42:56 -0700

    modified Main.cpp in motor input controll, now reads coltroller input and should change speed of motors, but currently no can signal is being sent

--- Motor_Input_Control/src/Main.cpp
+++ Motor_Input_Control/src/Main.cpp
@@ -4,6 +4,53 @@
 #include "can_control.h"
 #include "motor.h"
 
+#include<SDL2/SDL.h>
+
+SDL_Joystick* setup()
+{
+    // Initialize SDL with joystick support
+    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_JOYSTICK) < 0)
+    {
+        std::cerr << "SDL could not initialize! SDL_Error: "
+                  << SDL_GetError() << std::endl;
+        return nullptr;
+    }
+
+    // Check for joysticks
+    int joystickCount = SDL_NumJoysticks();
+    if (joystickCount < 1)
+    {
+        std::cout << "No joystick detected. Please connect your Logitech controller."
+                  << std::endl;
+        SDL_Quit();
+        return nullptr;
+    }
+
+    // Open first joystick
+    SDL_Joystick* joystick = SDL_JoystickOpen(0);
+    if (!joystick)
+    {
+        std::cerr << "Failed to open joystick! SDL_Error: "
+                  << SDL_GetError() << std::endl;
+        SDL_Quit();
+        return nullptr;
+    }
+
+    std::cout << "Detected controller: "
+              << SDL_JoystickName(joystick) << std::endl;
+
+    std::cout << "Number of axes: "
+              << SDL_JoystickNumAxes(joystick) << std::endl;
+
+    std::cout << "Number of buttons: "
+              << SDL_JoystickNumButtons(joystick) << std::endl;
+
+    std::cout << "Number of hats (D-pad): "
+              << SDL_JoystickNumHats(joystick) << std::endl;
+
+    return joystick;
+}
+
 int main() {
     // Initialize CAN interface
     CanDriver can("can0"); // Replace "can0" with your interface
@@ -26,7 +73,7 @@ int main() {
     f_right.start();
     b_left.start();
     b_right.start();
-
+/*
     //Set motor speed
     int test_speed = 5000; // ERPM
     std::cout << "Setting speed to " << test_speed << " ERPM" << std::endl;
@@ -50,11 +97,58 @@ int main() {
 
     //wait for 2 seconds
     std::this_thread::sleep_for(std::chrono::seconds(2));
+*/
 
+    SDL_Joystick* joystick = setup();
+	bool running = true;
+       SDL_Event event;
+     while(running){
+//    std::cout<<"doing stuff";
+    while (SDL_PollEvent(&event)){
+        if (event.type == SDL_JOYAXISMOTION){
+                int axis = event.jaxis.axis;
+
+                // SDL axis values are -32768 to 32767
+                float value = event.jaxis.value / 32767.0f;
+		 if(axis == 1){
+		     f_left.setSpeed(value*5000);
+		     b_left.setSpeed(value*5000);
+		 }else if (axis == 4){
+		     f_right.setSpeed(value*5000);
+		     b_right.setSpeed(value*5000);
+		 }
+                //std::cout << "Axis " << axis << " moved to " << value << std::endl;
+
+                /*
+                Tank Drive:
+                    Axis 1  -> left motors (up/down)
+                    Axis 3  -> right motors (up/down)
+
+                Arcade Drive:
+                    Axis 1  -> speed
+                    Axis 0  -> direction
+
+                Racing Drive:
+                    Axis 5  -> forward (right trigger)
+                    Axis 4  -> reverse (left trigger)
+                    Axis 0  -> steering
+                */
+            }
+            else if (event.type == SDL_JOYBUTTONDOWN)
+            {
+                 if((int)event.jbutton.button == 1){
+                 	running = false;
+                       std::cout << "ending"<<std::endl;
+                 }
+            }
+        }
+        }
 
     //Stop the motor
     std::cout << "Stopping motor..." << std::endl;
 
+    SDL_JoystickClose(joystick);
+    SDL_Quit();
     std::cout << "Motor stopped successfully." << std::endl;
 
     return 0;
